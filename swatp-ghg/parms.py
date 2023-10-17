643d125dacfbb2d2fb599f478dd436834bd69d43
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-10-17 18:06:35 -0500

    updated parms

---
+++ swatp-ghg/parms.py
@@ -0,0 +1,13 @@
+
+class DCparms(object):
+    
+    def __init__(self):
+        self.aeh = 0.23 # range: 
+        self.deh = 0.16 # range:
+        self.beh_flood = -250 # Lower limit value for Eh during flooding course (mv)
+        self.beh_drain = 300
+        self.zero_root_frac = 0.7 # (0-1)
+        self.ch4rootlim = 1.0 
+
+
+
