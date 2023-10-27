7499772caeb6bbb2f54c4e8d422868e5397d04df
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-10-27 10:33:59 -0500

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../ghgpy"))
+sys.path.insert(0, os.path.abspath(".."))
 
 project = 'ghgpy documentation'
 copyright = '2023, Seonggyu Park'
