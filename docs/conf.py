c8a8ba77c99664b50d1fa1a2dcfe843781354d6c
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-10-27 10:43:03 -0500

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,12 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.todo", 
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon" 
+    # "sphinx.ext.autodoc"
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
