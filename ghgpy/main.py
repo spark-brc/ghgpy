e7dfb600fdd3e2395ac387bdeef741b4b0fdb439
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-10-27 20:32:45 -0500

    check

--- ghgpy/main.py
+++ ghgpy/main.py
@@ -6,10 +6,7 @@ This file is part of ghgpy.
 This module contains a framework to summarize the total CH4 flux balance from rice paddy.
 
 .. math::
-
-   `\frac{ \sum_{t=0}^{N}f(t,k) }{N}`
-
-:math:`T_{CH4}=P_{CH4}-O_{CH4}+E_{CH4}+D_{CH4}+A_{CH4}`
+    \frac{ \sum_{t=0}^{N}f(t,k) }{N}
 
 """
 
